d14143c166066673826971d732c182da4f3b22cb
Pariasadat Hosseiny <p.hosseiny72@gmail.com>
2021-02-06 08:26:10 +0330

    first commit after creation

---
+++ src/main/java/com/zuhlke/ctt/CalculatorTestToolApplication.java
@@ -0,0 +1,13 @@
+package com.zuhlke.ctt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestToolApplication.class, args);
+	}
+
+}
